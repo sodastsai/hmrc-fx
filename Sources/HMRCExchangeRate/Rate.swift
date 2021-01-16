cb288429a2bf8b64a649131573eca3e04cfcf553
Tien-Che Tsai <sodas@icloud.com>
2021-01-16 16:27:12 +0000

    Add data type for Rate

--- Sources/HMRCExchangeRate/Rate.swift
+++ Sources/HMRCExchangeRate/Rate.swift
@@ -1,3 +1,31 @@
-struct Rate {
-  var text = "Hello, World!"
+import Foundation
+
+public struct Country {
+  public let name: String
+  public let code: String
+}
+
+public struct Currency {
+  public let name: String
+  public let code: String
+}
+
+public struct Rate {
+  public let country: Country
+  public let currency: Currency
+  public let rate: Decimal
+}
+
+extension Country: CustomStringConvertible {
+  public var description: String { name }
+}
+
+extension Currency: CustomStringConvertible {
+  public var description: String { code }
+}
+
+extension Rate: CustomStringConvertible {
+  public var description: String {
+    "GBP/\(currency) @ \(rate)"
+  }
 }
--- Tests/HMRCExchangeRateTests/RateTests.swift
+++ Tests/HMRCExchangeRateTests/RateTests.swift
@@ -2,7 +2,24 @@
 import XCTest
 
 final class RateTests: XCTestCase {
-  func testExample() {
-    XCTAssertEqual(Rate().text, "Hello, World!")
+  func testCountryDescription() {
+    let country = Country(name: "Taiwan", code: "TW")
+    XCTAssertEqual("\(country)", "Taiwan")
+  }
+
+  func testCurrencyDescription() {
+    let currency = Currency(name: "Dollar", code: "USD")
+    XCTAssertEqual("\(currency)", "USD")
+  }
+
+  func testRateDescription() {
+    guard let rawRate = Decimal(string: "39.84") else {
+      XCTFail("Couldn't inititalize Decimal for testing")
+      return
+    }
+    let rate = Rate(country: .init(name: "Taiwan", code: "TW"),
+                    currency: .init(name: "Dollar", code: "TWD"),
+                    rate: rawRate)
+    XCTAssertEqual("\(rate)", "GBP/TWD @ 39.84")
   }
 }
