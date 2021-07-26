aebbd4aedd2d9cacd017ea41484f3b1961826103
Tien-Che Tsai <sodas@icloud.com>
2021-07-26 23:30:55 +0100

    Remove redundant test case in HMRCSourceTests

--- Tests/HMRCExchangeRateTests/HMRCSourceTests.swift
+++ Tests/HMRCExchangeRateTests/HMRCSourceTests.swift
@@ -9,10 +9,4 @@ final class HMRCSourceTests: XCTestCase {
       "http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-0121.XML"
     )
   }
-
-  func testA() {
-    if let rate = RateSource.directHMRC.rate(of: "USD", in: .init(.dec, in: 2020))?.first {
-      print(rate)
-    }
-  }
 }
