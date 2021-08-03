b8f75686ca35f0444dec744bf35d8e4a4e61a66e
Tien-Che Tsai <sodas@icloud.com>
2021-08-03 22:57:32 +0100

    Update comment in the workaround to make @main work

--- Sources/HMRCExchangeRateDemo/Workaround.swift
+++ Sources/HMRCExchangeRateDemo/Workaround.swift
@@ -1,1 +1,2 @@
-// This is a temp workaround for @main to work
+// This is a temp workaround for @main to work.
+// Basically Swift now doesn't parse a module as a library if there's only one source file.
