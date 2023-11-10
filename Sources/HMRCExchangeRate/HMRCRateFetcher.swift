e94da15f008a9115c6e2ac2643ba93b131cd92c7
Tien-Che Tsai <sodas@icloud.com>
2023-11-10 23:58:05 +0000

    Update HMRC url paths

--- Sources/HMRCExchangeRate/HMRCRateFetcher.swift
+++ Sources/HMRCExchangeRate/HMRCRateFetcher.swift
@@ -11,9 +11,13 @@ struct HMRCRateFetcher: RemoteXMLDataRateFetcher {
   }
 
   func urlForRateXML(of month: Month) throws -> URL {
-    guard let url =
-      URL(string: "http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-\(string(of: month)).XML")
-    else {
+    let urlString = switch month {
+    case _ where month.year >= 2023:
+      "https://www.trade-tariff.service.gov.uk/api/v2/exchange_rates/files/monthly_xml_\(month.year)-\(month.name.rawValue).xml"
+    default:
+      "http://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-\(string(of: month)).XML"
+    }
+    guard let url = URL(string: urlString) else {
       throw RateFetcherError.invalidURL
     }
     return url
